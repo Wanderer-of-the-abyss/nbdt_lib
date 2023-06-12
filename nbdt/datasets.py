4ab5624049eb3a170ef127fd178a24315726aa08
Subhankar Panda <91069648+Wanderer-of-the-abyss@users.noreply.github.com>
2023-06-13 02:01:40 +0530

    Update datasets.py

--- nbdt/datasets.py
+++ nbdt/datasets.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-def load_dataset(dataset_name, destination_path):
+def load_dataset(dataset_name, destination_path=None):
     # Mapping of dataset names to dataset URLs
     dataset_mapping = {
         'arxiv': 'https://huggingface.co/datasets/PenguinMan/ARXIV/resolve/main/arxiv2.csv',
@@ -11,7 +11,16 @@ def load_dataset(dataset_name, destination_path):
 
     if dataset_name in dataset_mapping:
         dataset_url = dataset_mapping[dataset_name]
-        urllib.request.urlretrieve(dataset_url, destination_path)
-        print('Dataset downloaded successfully.')
+        
+        if destination_path is None:
+            # If destination_path is not specified, it will store the dataset in a variable
+            response = urllib.request.urlopen(dataset_url)
+            dataset_content = response.read().decode('utf-8')
+            print('Dataset downloaded successfully.') # print confirmation
+            return dataset_content
+        else:
+            urllib.request.urlretrieve(dataset_url, destination_path)
+            print('Dataset downloaded successfully.')
     else:
         print(f'Dataset "{dataset_name}" is not available.')
+
