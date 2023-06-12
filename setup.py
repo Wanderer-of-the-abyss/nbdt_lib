015c080fe5903cd6d01c6ce7dac039579ff2b8ea
Subhankar Panda <91069648+Wanderer-of-the-abyss@users.noreply.github.com>
2023-06-13 02:48:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='nbdt',
     version='0.1',
-    author='Subhankar',
+    author='Subhankar Panda',
     author_email='subhankarpanda556@example.com',
     description='nbdt library for reccomending authors, papers, and journals',
     packages=find_packages(),
     install_requires=[
         'wget',
+        'python'
     ],
 )
