3846c56cc1c6acf48a7e1b64ac08a8c117aca08b
Subhankar Panda <91069648+Wanderer-of-the-abyss@users.noreply.github.com>
2023-06-13 18:14:46 +0530

    Create test_nbdt.py

---
+++ tests/test_nbdt.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+from nbdt.datasets import load_dataset
+
+class TestNbdt(unittest.TestCase):
+
+    def setUp(self):
+        self.dataset_name = 'arxiv'
+        self.destination_path = 'data/arxiv_dataset.csv'
+    
+    def tearDown(self):
+        # Clean up any downloaded files after each test
+        if os.path.exists(self.destination_path):
+            os.remove(self.destination_path)
+    
+    def test_data_loader_success(self):
+        load_dataset(self.dataset_name, self.destination_path)
+        self.assertTrue(os.path.exists(self.destination_path))
+    
+    def test_data_loader_invalid_dataset(self):
+        invalid_dataset = 'invalid'
+        with self.assertRaises(ValueError):
+            load_dataset(invalid_dataset, self.destination_path)
+    
+    def test_data_loader_invalid_destination(self):
+        invalid_destination = 'na_folder/na_file.csv'
+        with self.assertRaises(FileNotFoundError):
+            load_dataset(self.dataset_name, invalid_destination)
+
+if __name__ == '__main__':
+    unittest.main()
